2d629eef10befd35cf53aad37a233d8b83e353ea
RodZhang <zhangrd_fight@icloud.com>
2018-12-02 03:22:43 +0800

    add FileUtil

--- lib/src/main/java/com/rod/halo/FileUtil.kt
+++ lib/src/main/java/com/rod/halo/FileUtil.kt
@@ -1,9 +1,16 @@
 package com.rod.halo
 
+import java.io.File
+
 /**
  *
  * @author Rod
  * @date 2018/12/2
  */
 object FileUtil {
+
+    fun isFileExist(path: String): Boolean {
+        val file = File(path)
+        return file.exists()
+    }
 }
