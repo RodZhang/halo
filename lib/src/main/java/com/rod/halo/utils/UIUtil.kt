23a4eaa286b56ce4d51c5c57f5251878855b9b8b
RodZhang <zhangrd_fight@icloud.com>
2020-06-02 01:19:43 +0800

    update

--- lib/src/main/java/com/rod/halo/utils/UIUtil.kt
+++ lib/src/main/java/com/rod/halo/utils/UIUtil.kt
@@ -1,7 +1,9 @@
 package com.rod.halo.utils
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 
 /**
  *
@@ -14,6 +16,12 @@ object UIUtil {
         if (view != null && view.parent is ViewGroup) {
             val parent = view.parent as ViewGroup
             parent.removeView(view)
+        }
+    }
+
+    fun showKeyboard(view: View?) {
+        view?.run {
+            (context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).showSoftInput(view, 0)
         }
     }
 }
